718b049f9bb05435f56e8bc3e5bb5ba78e3ce2d0
Damaris Göbel <damaris.goebel@gmx.de>
2021-01-03 20:29:06 +0100

    deploy

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './components/Card'
+
+function App() {
+  return (
+  <div>
+
+    <head>
+
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+      <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&display=swap" rel="stylesheet"/>
+    </head>
+
+    <header className="header" >
+     <div>Give a little joy cards</div>
+    </header>
+
+    <Card/>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/assets/data/image.js
@@ -0,0 +1,229 @@
+let images = [
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699302/cards/birth_1_ndd7fk.jpg",  
+"category": "birth"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699308/cards/birth_2_buj3vf.jpg",  
+"category": "birth"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699312/cards/birthdays_1_mnaucs.jpg",  
+"category": "birthdays"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699312/cards/birthdays_2_dxoqb4.jpg",  
+"category": "birthdays"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699300/cards/birthdays_3_mlz5fg.jpg",  
+"category": "birthdays"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699290/cards/christmas_1_njtf7x.jpg",  
+"category": "christmas"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699316/cards/christmas_2_upubaz.jpg",  
+"category": "christmas"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699313/cards/christmas_3_ftx2ji.jpg",  
+"category": "christmas"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699324/cards/christmas_4_axmrgm.jpg",  
+"category": "christmas"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699325/cards/friendship_1_zxi0pv.jpg",  
+"category": "friendship"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699317/cards/friendship_2_rg8gck.jpg",  
+"category": "friendship"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699317/cards/friendship_3_lfw0nm.jpg",  
+"category": "friendship"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699329/cards/friendship_4_zvvwdt.jpg",  
+"category": "friendship"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699316/cards/friendship_5_bulddy.jpg",  
+"category": "friendship"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699323/cards/friendship_6_uyuojl.jpg",  
+"category": "friendship"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699311/cards/friendship_7_mrqyji.jpg",  
+"category": "friendship"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699323/cards/friendship_8_crk8wm.jpg",  
+"category": "friendship"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699324/cards/friendship_9_setjt8.jpg",  
+"category": "friendship"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699315/cards/friendship_10_svthya.jpg",  
+"category": "friendship"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699314/cards/friendship_11_vwhjyc.jpg",  
+"category": "friendship"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699307/cards/friendship_12_osiiv1.jpg",  
+"category": "friendship"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699306/cards/friendship_13_gtt6ch.jpg",  
+"category": "friendship"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699316/cards/friendship_14_x6jyuy.jpg",  
+"category": "friendship"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699296/cards/friendship_15_sftv8j.jpg",  
+"category": "friendship"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699300/cards/friendship_16_r9au5j.jpg",  
+"category": "friendship"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699291/cards/friendship_17_oczb32.jpg",  
+"category": "friendship"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699292/cards/friendship_18_wtgtxp.jpg",  
+"category": "friendship"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699332/cards/friendship_19_konehe.jpg",  
+"category": "friendship"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699337/cards/friendship_20_inj2hg.jpg",  
+"category": "friendship"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699303/cards/misc_1_vg7fhg.jpg",  
+"category": "misc"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699294/cards/misc_2_wlhm4h.jpg",  
+"category": "misc"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699292/cards/misc_3_ncb09p.jpg",  
+"category": "misc"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699299/cards/misc_4_etlott.jpg",  
+"category": "misc"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699333/cards/misc_5_wsv46j.jpg",  
+"category": "misc"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699329/cards/misc_6_rzjros.jpg",  
+"category": "misc"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699333/cards/misc_7_eh72qg.jpg",  
+"category": "misc"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699290/cards/misc_8_invd2t.jpg",  
+"category": "misc"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699334/cards/misc_9_uaa9hg.jpg",  
+"category": "misc"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699325/cards/misc_10_lnizj0.jpg",  
+"category": "misc"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699324/cards/nerdy_1_fbtfm1.jpg",  
+"category": "nerdy"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699320/cards/partnership_1_ynkfc5.jpg",  
+"category": "partnership"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699317/cards/partnership_2_raxbm9.jpg",  
+"category": "partnership"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699325/cards/partnership_3_dgtgn2.jpg",  
+"category": "partnership"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699311/cards/partnership_4_a9mydq.jpg",  
+"category": "partnership"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699320/cards/partnership_5_vpsxkw.jpg",  
+"category": "partnership"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699307/cards/partnership_6_i5n1fr.jpg",  
+"category": "partnership"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699302/cards/partnership_7_xdvewu.jpg",  
+"category": "partnership"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699297/cards/partnership_8_eldvv7.jpg",  
+"category": "partnership"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699307/cards/partnership_9_snoetf.jpg",  
+"category": "partnership"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699294/cards/partnership_10_enu06m.jpg",  
+"category": "partnership"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699294/cards/partnership_11_rnd3w8.jpg",  
+"category": "partnership"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699295/cards/partnership_12_cocbab.jpg",  
+"category": "partnership"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699295/cards/partnership_13_d2t0co.jpg",  
+"category": "partnership"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699333/cards/partnership_14_bbkgqy.jpg",  
+"category": "partnership"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699334/cards/partnership_15_rvss6m.jpg",  
+"category": "partnership"
+},
+{
+"image": "https://res.cloudinary.com/dfx4shmcy/image/upload/v1609699329/cards/partnership_16_oklsvi.jpg",  
+"category": "partnership"
+}
+]
+
+export default images
+
